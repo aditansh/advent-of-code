9324a54e5010f19bd937ac7e692e70c243fec570
Aaditya Mahanta <aadityamahanta07@gmail.com>
2023-12-03 11:56:22 +0530

    gitignore

--- day2/target/debug/day2.d
+++ day2/target/debug/day2.d
@@ -1,1 +1,1 @@
-E:\VS\ CODE\aoc\day2\target\debug\day2.exe: E:\VS\ CODE\aoc\day2\src\main.rs
+E:\VS\ CODE\advent-of-code\day2\target\debug\day2.exe: E:\VS\ CODE\advent-of-code\day2\src\main.rs
--- day2/target/debug/deps/day2-6a12ca99a12e97ae.d
+++
@@ -1,5 +0,0 @@
-E:\VS CODE\aoc\day2\target\debug\deps\day2-6a12ca99a12e97ae.exe: src\main.rs
-
-E:\VS CODE\aoc\day2\target\debug\deps\day2-6a12ca99a12e97ae.d: src\main.rs
-
-src\main.rs:
